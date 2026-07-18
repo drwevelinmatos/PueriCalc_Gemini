c25c07c560de4b472da9717e2f3a587d7397071f
Dr. Wevelin Matos <drmatos.cardio@gmail.com>
2026-07-18 16:33:24 -0300

    Update ecg.js

--- js/tabs/cardio/ecg.js
+++ js/tabs/cardio/ecg.js
@@ -150,15 +150,15 @@ export function initECGCard() {
 
                 <img src="./assets/marca-dagua-a5.png" style="position: absolute; top: 50%; right: 0; transform: translateY(-50%); opacity: 0.15; pointer-events: none; z-index: 1; max-width: 120mm;">
 
-                <div style="flex-grow: 1; position: absolute; padding: 5mm 10mm 50mm 10mm; z-index: 10; font-size: 12px; box-sizing: border-box;">
+                <div style="flex-grow: 1; position: absolute; padding: 10mm 10mm 50mm 10mm; z-index: 10; font-size: 12px; box-sizing: border-box;">
                     <h1 style="text-align: center; font-size: 12px; font-weight: bold; margin-bottom: 20px; letter-spacing: 1px; text-decoration: underline;">ELETROCARDIOGRAMA</h1>
                     
                     <div id="a5-content">
                         </div>
                 </div>
 
-                <div style="position:absolute; width: 100%; line-height: 0; margin-bottom: 0;">
-                    <img src="./assets/rodape-a5.png" style="width: 100%; height: auto; display: block; margin-bottom: 0;">
+                <div style="position:absolute; width: 100%; line-height: 0; bottom: 0;">
+                    <img src="./assets/rodape-a5.png" style="width: 100%; height: auto; display: block; bottom: 0;">
                 </div>
             </div>
 
